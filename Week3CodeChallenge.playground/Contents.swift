f54e3378e5afdd5a338d03dab5846672ea051f1a
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-06-21 16:38:44 -0700

    complete code challenge

--- Week3CodeChallenge.playground/Contents.swift
+++ Week3CodeChallenge.playground/Contents.swift
@@ -3,4 +3,25 @@
 import UIKit
 
 // Write a function that determines how many words there are in a sentence
+func wordCount(string: String) -> Int {
+    let sentence = string.componentsSeparatedByString(" ")
+    return sentence.count
+}
+
+let string = "How many words are here?"
+wordCount(string)
+
 // Write a function that returns all the odd elements of an array
+func isOdd(data: [Int]) -> [Int] {
+    var odd = [Int]()
+    for num in data {
+        if !(num % 2 == 0) {
+        odd.append(num)
+        }
+    }
+    return odd
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+isOdd(array)
