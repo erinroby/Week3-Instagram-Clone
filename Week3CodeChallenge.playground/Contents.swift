c5cd58f428e70c4101819094de61077813762c51
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-06-26 19:23:49 -0700

    Friday lab whiteboarding

--- Week3CodeChallenge.playground/Contents.swift
+++ Week3CodeChallenge.playground/Contents.swift
@@ -47,5 +47,55 @@ func fibonacci() -> [Double] {
 
 fibonacci()
 
+// Friday whiteboarding with Sean
+
+func random(longLink: String, length: Int) -> String {
+    let base = longLink
+    var shortLink: String = ""
+    
+    for _ in 0..<length {
+        let randomChar = arc4random_uniform(UInt32(base.characters.count))
+        shortLink += "\(base[base.startIndex.advancedBy(Int(randomChar))])"
+    }
+    return shortLink
+}
+
+var links = [String : String]()
+
+func generate(longLink: String) -> String {
+    var count = 0
+    let shortLink = links[longLink]
+    
+    if shortLink == nil {
+        links[longLink] = random(longLink, length: 6)
+        count += 1
+    }
+    
+    for (key, value) in links {
+        if longLink == key {
+            links.removeValueForKey(value)
+            links[longLink] = value
+            return value
+        } else if value == shortLink {
+            generate(longLink)
+        }
+    }
+    
+    if count >= 100 {
+        links.removeValueForKey(shortLink!)
+    }
+
+    return shortLink!
+}
+
+// tests
+generate("www.facebook.com")
+generate("www.facebook.com")
+generate("www.facebook.com")
+generate("www.facebook.com")
+generate("www.linkedin.com")
+
+print(links)
+
 
 
